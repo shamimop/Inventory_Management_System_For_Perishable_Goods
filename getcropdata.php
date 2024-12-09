091c64ad3830e8fe19543d7766a04b5ec413cf30
porom64 <poromaroytoma@gmail.com>
2024-12-09 12:35:33 +0600

    front end chng
    chng to frontend

---
+++ getcropdata.php
@@ -0,0 +1,25 @@
+<?php
+include 'database.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $CropID = $conn->real_escape_string($_POST['CropID']);
+    $idate = $conn->real_escape_string($_POST['visitdate']);
+    $feedback = $conn->real_escape_string($_POST['feedback']);
+    $cropQuality = $conn->real_escape_string($_POST['Quality']);
+    $farmid = $conn->real_escape_string($_POST['fid']);
+
+    
+    
+    
+    $sql = "INSERT INTO `tblfarmsurvey` (`AgriOfficerId`, `Inspection_Date`, `Feedback`, `Crop_Quality`, `Farm_id`) 
+        VALUES ('$AgriOfficerId', '$idate', '$feedback', '$cropQuality', '$farmid')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data saved successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+}
+?>
