9c131e16655aad8fa6df5d4367f61be72ca35da6
Sadia Alam <sadi15156@gmail.com>
2024-12-17 11:54:38 +0600

    code changes

--- getcropdata.php
+++ getcropdata.php
@@ -5,12 +5,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize and fetch input values
     $cropID = $conn->real_escape_string($_POST['cropID']);
     $cropName = $conn->real_escape_string($_POST['cropName']);
-    $minimumTemperature = $conn->real_escape_string($_POST['minimum Temperature']);
+    $minimumTemperature = $conn->real_escape_string($_POST['minimumTemperature']);
     $minimumHumidity = $conn->real_escape_string($_POST['minimumHumidity']);
     $cropSeason = $conn->real_escape_string($_POST['cropSeason']);
 
     // Insert data into the database
-    $sql = "INSERT INTO crop_t (cropID, cropName, minimum Temperature, minimumHumidity, cropSeason) 
+    $sql = "INSERT INTO crop_t (cropID, cropName, minimumTemperature, minimumHumidity, cropSeason) 
             VALUES ('$cropID', '$cropName', '$minimumTemperature', '$minimumHumidity', '$cropSeason')";
 
     if ($conn->query($sql) === TRUE) {
